d0cfa21d218eabf91759cd163fb971c61bafa0dc
vicsurcor <vdsurcor@gmail.com>
2024-01-09 20:11:25 +0100

    init

---
+++ Primeras_Vistas_VictorSuros/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Primeras_Vistas"
+include(":app")
+ 
